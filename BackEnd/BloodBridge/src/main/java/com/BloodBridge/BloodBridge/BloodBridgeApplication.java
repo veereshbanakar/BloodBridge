b88ae095ef9cac14bcad73bfc39a941b8a3d0dec
veereshbanakar <veereshbanakar821@gmail.com>
2025-06-10 19:27:20 +0530

    backend maven project added

---
+++ BackEnd/BloodBridge/src/main/java/com/BloodBridge/BloodBridge/BloodBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.BloodBridge.BloodBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBridgeApplication.class, args);
+	}
+
+}
