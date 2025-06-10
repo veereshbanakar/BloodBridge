9c7902ef66f1415d17fb4a986b830c623b6093e8
veereshbanakar <veereshbanakar821@gmail.com>
2025-06-10 20:27:59 +0530

    files created authrepo authservice

---
+++ BackEnd/BloodBridge/src/main/java/com/BloodBridge/BloodBridge/Repository/AuthRepository.java
@@ -0,0 +1,5 @@
+package com.BloodBridge.BloodBridge.Repository;
+
+public class AuthRepository {
+
+}
---
+++ BackEnd/BloodBridge/src/main/java/com/BloodBridge/BloodBridge/Service/AuthService.java
@@ -0,0 +1,5 @@
+package com.BloodBridge.BloodBridge.Service;
+
+public class AuthService {
+
+}
