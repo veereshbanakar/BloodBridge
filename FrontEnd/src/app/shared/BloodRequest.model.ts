801eb3069508e6b68c1d657e7caff34bb7bd0252
Anish <anishyadav7853@gmail.com>
2025-06-15 18:07:33 +0530

    created dashboard components

---
+++ FrontEnd/src/app/shared/BloodRequest.model.ts
@@ -0,0 +1,10 @@
+interface BloodRequest{
+    id: number,
+    bloodType: string,
+    urgency: string,
+    title: string,
+    description: string,
+    location: string,
+    distance: string,
+    timeAgo: string
+}
