560dc61e9a6be1e5e713a9c33ba9a1c6a0508a61
Anish <anishyadav7853@gmail.com>
2025-06-29 15:30:43 +0530

    fixed navbar bug

--- FrontEnd/src/app/pages/donor/donor-profile/donor-profile.component.ts
+++ FrontEnd/src/app/pages/donor/donor-profile/donor-profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { NavbarDonorComponent } from '../components/navbar-donor/navbar-donor.component';
 
 @Component({
   selector: 'app-donor-profile',
-  imports: [NavbarDonorComponent],
+  imports: [NavbarDonorComponent,RouterModule],
   templateUrl: './donor-profile.component.html',
   styleUrl: './donor-profile.component.css'
 })
