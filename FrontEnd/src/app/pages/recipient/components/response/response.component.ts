f3a6fdf38620a101e1a8665ace425ffbc4dc8644
prajeet-banna <prajeetsingh21@gmail.com>
2025-07-01 19:42:54 +0530

    donor_response_Added

---
+++ FrontEnd/src/app/pages/recipient/components/response/response.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-response',
+  imports: [],
+  templateUrl: './response.component.html',
+  styleUrl: './response.component.css'
+})
+export class ResponseComponent {
+
+}
